48e95e97f480b3c40d67d974763931b8bc359076
Josue Robledo <35754305+josuerobld@users.noreply.github.com>
2024-08-05 23:44:02 -0600

    cambios

--- script.js
+++ script.js
@@ -8,17 +8,33 @@ function reflex_agent(location, state){
 }
 
 function test(states){
-       var location = states[0];		
-       var state = states[0] == "A" ? states[1] : states[2];
-       var action_result = reflex_agent(location, state);
-       document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-       if (action_result == "CLEAN"){
-         if (location == "A") states[1] = "CLEAN";
-          else if (location == "B") states[2] = "CLEAN";
-       }
-       else if (action_result == "RIGHT") states[0] = "B";
-       else if (action_result == "LEFT") states[0] = "A";		
- setTimeout(function(){ test(states); }, 2000);
+    let cont = 1
+    while (true){
+      if (cont==9){
+        document.getElementById("log").innerHTML+="<br>Proceso finalizado: La maquina esta en resposo";
+        break;
+      }
+
+      var location = states[0];		
+      var state = states[0] == "A" ? states[1] : states[2];
+      var action_result = reflex_agent(location, state);
+      
+      document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" | Estado: ").concat(cont);
+      if(cont == 5){
+        //ensuciamos para que recorrar los estados
+        states[1] = "DIRTY";
+        states[2] = "DIRTY";  
+        location = "B"
+      }
+      if (action_result == "CLEAN"){
+        if (location == "A") states[1] = "CLEAN";
+        else if (location == "B") states[2] = "CLEAN";
+      }
+      else if (action_result == "RIGHT") states[0] = "B";
+      else if (action_result == "LEFT") states[0] = "A";		
+      cont += 1
+      setTimeout(2000)
+    }
 }
 
 var states = ["A","DIRTY","DIRTY"];
