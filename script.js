aeee2640d73f464bd2129c3de9ff09ec0faae633
Josue Robledo <35754305+josuerobld@users.noreply.github.com>
2024-08-05 23:47:06 -0600

    fixed

--- script.js
+++ script.js
@@ -1,40 +1,34 @@
-// MIT License
-// Copyright (c) 2020 Luis Espino
+function test(states) {
+  let cont = 1;
 
-function reflex_agent(location, state){
-    if (state=="DIRTY") return "CLEAN";
-    else if (location=="A") return "RIGHT";
-    else if (location=="B") return "LEFT";
-}
-
-function test(states){
-    let cont = 1
-    while (true){
-      if (cont==9){
-        document.getElementById("log").innerHTML+="<br>Proceso finalizado: La maquina esta en resposo";
-        break;
+  function loop() {
+      if (cont == 9) {
+          document.getElementById("log").innerHTML += "<br>Proceso finalizado: La maquina esta en resposo";
+          return;
       }
 
-      var location = states[0];		
+      var location = states[0];
       var state = states[0] == "A" ? states[1] : states[2];
       var action_result = reflex_agent(location, state);
-      
-      document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" | Estado: ").concat(cont);
-      if(cont == 5){
-        //ensuciamos para que recorrar los estados
-        states[1] = "DIRTY";
-        states[2] = "DIRTY";  
-        location = "B"
+
+      document.getElementById("log").innerHTML += "<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" | Estado: ").concat(cont);
+      if (cont == 5) {
+          //ensuciamos para que recorrar los estados
+          states[1] = "DIRTY";
+          states[2] = "DIRTY";
+          location = "B"
       }
-      if (action_result == "CLEAN"){
-        if (location == "A") states[1] = "CLEAN";
-        else if (location == "B") states[2] = "CLEAN";
-      }
-      else if (action_result == "RIGHT") states[0] = "B";
-      else if (action_result == "LEFT") states[0] = "A";		
-      cont += 1
-      setTimeout(2000)
-    }
+      if (action_result == "CLEAN") {
+          if (location == "A") states[1] = "CLEAN";
+          else if (location == "B") states[2] = "CLEAN";
+      } else if (action_result == "RIGHT") states[0] = "B";
+      else if (action_result == "LEFT") states[0] = "A";
+
+      cont += 1;
+      setTimeout(loop, 2000); // Schedule the next iteration
+  }
+
+  loop(); // Start the loop
 }
 
 var states = ["A","DIRTY","DIRTY"];
